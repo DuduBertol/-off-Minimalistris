fb20048a65e3729ca554a369dce97fbb3df4af55
DuduBertol <dudubertoldev@gmail.com>
2023-01-19 11:56:27 -0300

    Canvas Beautify
    Canvas game and buttons.
    Canvas Pause.
    Show Next Tetromino.

--- Minimalistris/Assets/SpawnTetromino.cs
+++ Minimalistris/Assets/SpawnTetromino.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class SpawnTetromino : MonoBehaviour
 {
     public GameObject[] Tetrominoes;
+    public List<GameObject> imagesTetrominoes = new List<GameObject>();
+    private int nextTetromino;
 
     // Start is called before the first frame update
     void Start()
     {
+        nextTetromino = Random.Range(0, Tetrominoes.Length);
         NewTetromino();
     }
 
     public void NewTetromino()
     {
-        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+        Instantiate(Tetrominoes[nextTetromino], transform.position, Quaternion.identity);
+        nextTetromino = Random.Range(0, Tetrominoes.Length);
+
+        for(int i = 0; i < imagesTetrominoes.Count; i++)
+        {
+            imagesTetrominoes[i].SetActive(false);
+        }
+
+        imagesTetrominoes[nextTetromino].SetActive(true);
     }
 }
