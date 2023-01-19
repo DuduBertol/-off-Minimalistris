1a2a6feca9c69bd82f3639964a20f958e7a4058f
DuduBertol <dudubertoldev@gmail.com>
2023-01-19 01:22:54 -0300

    Spawner, Line Breaker and Collisions
    Scripts to make spawn, collisions and the line breaker.

--- Minimalistris/Assets/TetrisBlock.cs
+++ Minimalistris/Assets/TetrisBlock.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class TetrisBlock : MonoBehaviour
 {
-    public GameManager gameManager;
+    private GameManager gameManager;
 
     public Vector3 rotationPoint;
     private float previousTime;
     public float fallTime = 0.8f;
     public static int height = 20;
     public static int width = 10;
+    private static Transform[,] grid = new Transform[width, height];
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
@@ -58,9 +59,75 @@ public class TetrisBlock : MonoBehaviour
             if(!ValidMove())
             {
                 transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckForLines();
+
+                this.enabled = false;
+                FindObjectOfType<SpawnTetromino>().NewTetromino();
             }  
             previousTime = Time.time;
             gameManager.downClick = false;
+        }
+    }
+
+    void CheckForLines()
+    {
+        for(int i = height - 1; i >= 0; i--)
+        {
+            if(HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if(grid[j, i] == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for(int y = i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if(grid[j,y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3 (0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
         }
     }
 
@@ -75,6 +142,9 @@ public class TetrisBlock : MonoBehaviour
             {
                 return false;
             }
+
+            if(grid[roundedX, roundedY] != null)
+                return false;
         }
 
         return true;
