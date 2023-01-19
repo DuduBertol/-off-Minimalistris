317d52519475897d1e94328878ac9b32dbdb0715
DuduBertol <dudubertoldev@gmail.com>
2023-01-19 00:11:38 -0300

    Movements, Grid and Buttons
    Scripts for Movementation, Grid, and Buttons

---
+++ Minimalistris/Assets/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool downClick;
+    public bool leftClick;
+    public bool rightClick;
+    public bool circleClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DownButton()
+    {
+        downClick = true;
+    }
+
+    public void LeftButton()
+    {
+        leftClick = true;
+    }
+
+    public void RightButton()
+    {
+        rightClick = true;
+    }
+    public void CircleButton()
+    {
+        circleClick = true;
+    }
+}
--- Minimalistris/Assets/TetrisBlock.cs
+++ Minimalistris/Assets/TetrisBlock.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class TetrisBlock : MonoBehaviour
 {
+    public GameManager gameManager;
+
+    public Vector3 rotationPoint;
+    private float previousTime;
+    public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,62 @@ public class TetrisBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameManager.rightClick)
+        {
+            transform.position += new Vector3(1, 0, 0);
+            gameManager.rightClick = false;
+            if(!ValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }  
+        }
+
+        if(gameManager.leftClick)
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            gameManager.leftClick = false;
+            if(!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }  
+        }
+
+        //ROTATE
+        if(gameManager.circleClick)
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3 (0, 0, 1), 90);
+            gameManager.circleClick = false;
+             if(!ValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3 (0, 0, 1), -90);
+            } 
+        }
+
+        if(Time.time - previousTime > (gameManager.downClick ? fallTime / 10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if(!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }  
+            previousTime = Time.time;
+            gameManager.downClick = false;
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
